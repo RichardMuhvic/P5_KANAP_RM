2f16c6f7dd216ee60e30e2962161e9223e1e2458
Richard M <richardmuhvic@gmail.com>
2023-03-07 00:05:01 +0100

    j'apporte des modification plus en détails sur la partie quantité

--- front/js/cart.js
+++ front/js/cart.js
@@ -2,7 +2,8 @@
 let productLocalStorage = JSON.parse(localStorage.getItem("product"));
 console.log(productLocalStorage);
 
-fetch(`http://localhost:3000/api/products`)
+
+fetch(`http://localhost:3000/api/products/`)
     .then((res) => {
         res.json()
         console.log("ça marche 1");
@@ -14,90 +15,98 @@ fetch(`http://localhost:3000/api/products`)
         console.log("Impossible de récupérer les données du produit", err);
     });
 
-//-------------------------------------------------------------------
-//----------Création de l'intégralité du block----------------------
-//-------------------------------------------------------------------
+//let panier = [];
 
-const sectionItem = document.querySelector("#cart__items");
-let itemArticle = document.createElement("article");
-sectionItem.appendChild(itemArticle);
-itemArticle.classList.add("cart__item");
+for (i =0; i < productLocalStorage.length; i++) {
+    console.log(productLocalStorage.length);
+    //-------------------------------------------------------------------
+    //----------Création de l'intégralité du block----------------------
+    //-------------------------------------------------------------------
+    const sectionItem = document.querySelector("#cart__items");
+    let itemArticle = document.createElement("article");
+    sectionItem.appendChild(itemArticle);
+    itemArticle.classList.add("cart__item");
 
-const sectionCartItem = document.querySelector(".cart__item");
-let itemCartImg = document.createElement("div");
-sectionCartItem.appendChild(itemCartImg);
-itemCartImg.classList.add("cart__item__img");
+    const sectionCartItem = document.querySelector(".cart__item");
+    let itemCartImg = document.createElement("div");
+    sectionCartItem.appendChild(itemCartImg);
+    itemCartImg.classList.add("cart__item__img");
 
-const sectionImg = document.querySelector(".cart__item__img");
-let itemImg = document.createElement("img");
-// A CETTE LIGNE Récupération de l'image depuis l'API du produit choisi dans le panier //
-sectionImg.appendChild(itemImg);
+    const sectionImg = document.querySelector(".cart__item__img");
+    let itemImg = document.createElement("img");
+    sectionImg.appendChild(itemImg);
+    // A CETTE LIGNE Récupération de l'image depuis l'API du produit choisi dans le panier //
+    itemImg.src = productLocalStorage[i].imageUrl;
+    //------------
 
-//--------------------------------------------------------------//
-// ----- création du bloc, div , cart__item__content ---------- //
-let itemCartContent = document.createElement("div");
-sectionCartItem.appendChild(itemCartContent);
-itemCartContent.classList.add("cart__item__content");
+    //--------------------------------------------------------------//
+    // ----- création du bloc, div , cart__item__content ---------- //
+    let itemCartContent = document.createElement("div");
+    sectionCartItem.appendChild(itemCartContent);
+    itemCartContent.classList.add("cart__item__content");
 
-//-----------------------------------------------------------------------//
-// ------ cration du bloc, div, cart__item__content__description --------//
-const sectionCartContent = document.querySelector(".cart__item__content");
-let itemCartContentDescription = document.createElement("div");
-sectionCartContent.appendChild(itemCartContentDescription);
-itemCartContentDescription.classList.add("cart__item__content__description");
+    //-----------------------------------------------------------------------//
+    // ------ cration du bloc, div, cart__item__content__description --------//
+    const sectionCartContent = document.querySelector(".cart__item__content");
+    let itemCartContentDescription = document.createElement("div");
+    sectionCartContent.appendChild(itemCartContentDescription);
+    itemCartContentDescription.classList.add("cart__item__content__description");
 
-const sectionNomProduit = document.querySelector(".cart__item__content__description");
-let itemNomProduit = document.createElement("h2");
-sectionNomProduit.appendChild(itemNomProduit);
-// CETTE LIGNE, récupération du nom du produit au loczaltstorage
-const sectionCouleurProduit = document.querySelector(".cart__item__content__description");
-let itemCouleurProduit = document.createElement("p");
-sectionCouleurProduit.appendChild(itemCouleurProduit);
-// CETTE LIGNE, récupération de la couleur choisi au localstorage
-const sectionPrixProduit = document.querySelector(".cart__item__content__description");
-let itemPrixProduit = document.createElement("p");
-sectionPrixProduit.appendChild(itemPrixProduit);
-// CETTE lIGNE, récupération du prix du produit au localstorage
+    const sectionNomProduit = document.querySelector(".cart__item__content__description");
+    let itemNomProduit = document.createElement("h2");
+    sectionNomProduit.appendChild(itemNomProduit);
+    // CETTE LIGNE, récupération du nom du produit au loczaltstorage
+    itemNomProduit.innerText = productLocalStorage[i].productTitle;
 
-//-----------------------------------------------------------------------//
-// ----- création du bloc, div, cart__item__content__settings------------//
-const sectionCartContentSetting = document.querySelector(".cart__item__content");
-let itemCartContentSettings = document.createElement("div");
-sectionCartContentSetting.appendChild(itemCartContentSettings);
-itemCartContentSettings.classList.add("cart__item__content__settings");
+    const sectionCouleurProduit = document.querySelector(".cart__item__content__description");
+    let itemCouleurProduit = document.createElement("p");
+    sectionCouleurProduit.appendChild(itemCouleurProduit);
+    // CETTE LIGNE, récupération de la couleur choisi au localstorage
+    itemCouleurProduit.innerText = productLocalStorage[i].productColors ;
 
-let itemCartContentSettingsQuantity = document.createElement("div");
-sectionCartContentSetting.appendChild(itemCartContentSettingsQuantity);
-itemCartContentSettingsQuantity.classList.add("cart__item__content__settings__quantity");
+    const sectionPrixProduit = document.querySelector(".cart__item__content__description");
+    let itemPrixProduit = document.createElement("p");
+    sectionPrixProduit.appendChild(itemPrixProduit);
+    // CETTE lIGNE, récupération du prix du produit au localstorage
+    itemPrixProduit.innerText = productLocalStorage[i].productPrice;
 
-const sectionQuantityProduit = document.querySelector(".cart__item__content__settings__quantity");
-let itemQuantityProduit = document.createElement("p");
-sectionQuantityProduit.appendChild(itemQuantityProduit);
-// CETTE LIGNE, récupération de la quantité du produit au loczaltstorage
-const sectionChangementQuantityProduit = document.querySelector(".cart__item__content__settings__quantity");
-let itemChangementQuantityProduit = document.createElement("input");
-sectionChangementQuantityProduit.appendChild(itemChangementQuantityProduit);
+    //-----------------------------------------------------------------------//
+    // ----- création du bloc, div, cart__item__content__settings------------//
+    const sectionCartContentSetting = document.querySelector(".cart__item__content");
+    let itemCartContentSettings = document.createElement("div");
+    sectionCartContentSetting.appendChild(itemCartContentSettings);
+    itemCartContentSettings.classList.add("cart__item__content__settings");
 
-//---------------------------------------------------------------------------//
-// ----- création du bloc, div, cart__item__content__settings__delete ---//
-const sectionCartItemContentSettingsDelete = document.querySelector(".cart__item__content__settings");
-let itemCartContentSettingsDelete = document.createElement("div");
-sectionCartItemContentSettingsDelete.appendChild(itemCartContentSettingsDelete);
-itemCartContentSettingsDelete.classList.add("cart__item__content__settings__delete");
+    let itemCartContentSettingsQuantity = document.createElement("div");
+    sectionCartContentSetting.appendChild(itemCartContentSettingsQuantity);
+    itemCartContentSettingsQuantity.classList.add("cart__item__content__settings__quantity");
 
-const sectionBtnDeleteItem = document.querySelector(".cart__item__content__settings__delete");
-let deleteItem = document.createElement("p");
-sectionBtnDeleteItem.appendChild(deleteItem);
-deleteItem.classList.add(".deleteItem");
 
-console.log(sectionItem);
-console.log(sectionCartItem)
-console.log(sectionImg)
-console.log(sectionCartContent)
-console.log(sectionNomProduit)
-console.log(sectionCouleurProduit)
-console.log(sectionCartContentSetting)
-console.log(sectionQuantityProduit)
-console.log(sectionChangementQuantityProduit)
-console.log(sectionCartItemContentSettingsDelete)
-console.log(sectionBtnDeleteItem)
+
+    const sectionQuantityProduit = document.querySelector(".cart__item__content__settings__quantity");
+    let itemQuantityProduit = document.createElement("p");
+    sectionQuantityProduit.appendChild(itemQuantityProduit);
+
+    const sectionInputQuantity = document.querySelector(".cart__item__content__settings__quantity");
+    let itemInputQuantity = document.createElement("input");
+    sectionInputQuantity.appendChild(itemInputQuantity);
+    itemInputQuantity.classList.add("itemQuantity");
+    // CETTE LIGNE, récupération de la quantité du produit au localtstorage
+    itemInputQuantity.value = productLocalStorage[i].productQuantity;
+
+
+    //---------------------------------------------------------------------------//
+    // ----- création du bloc, div, cart__item__content__settings__delete ---//
+    const sectionCartItemContentSettingsDelete = document.querySelector(".cart__item__content__settings");
+    let itemCartContentSettingsDelete = document.createElement("div");
+    sectionCartItemContentSettingsDelete.appendChild(itemCartContentSettingsDelete);
+    itemCartContentSettingsDelete.classList.add("cart__item__content__settings__delete");
+
+    const sectionBtnDeleteItem = document.querySelector(".cart__item__content__settings__delete");
+    let deleteItem = document.createElement("p");
+    sectionBtnDeleteItem.appendChild(deleteItem);
+    deleteItem.classList.add(".deleteItem");
+
+    console.log(sectionItem);
+    console.log(sectionCartItem)
+};
